f939b33b5afa0620dfcb865ff4238a3e45e5b65e
girish-fuluskar <girish.fuluskar@gmail.com>
2017-02-02 12:42:47 +0530

    updated code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,17 +2,28 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
 
+  $scope.getcolor = function(brightness) {
+    var rgb = [Math.random() * 0, Math.random() * 255, Math.random() * 255];
+    var mix = [brightness * 51, brightness * 51, brightness * 51]; //51 => 255/5
+    var mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map(function(x) {
+        return Math.round(x / 2.0)
+    })
+    return "rgba(" + mixedrgb.join(",") + ",0.9)";
+    //return "rgba("+(Math.floor(Math.random() * 255))+", "+(Math.floor(Math.random() * 255))+","+(Math.floor(Math.random() * 255))+",0.8)";
+    //return '#' + Math.random().toString(16).substr(-6);
+  }
+
   $scope.getBarGraphData = function(){
-    var labels = ['A1','B1','C1','D1','E1'];
-    var name = ['K1','K2','K3','K4','K5'];
-    var data = [["10","120","70","30","190"],["10","120","70","30","190"],["10","120","70","30","190"],["10","120","70","30","190"],["10","120","70","30","190"]];
+    var labels = ['A1','B1','C1','D1','E1','F1','G1','H1'];
+    var name = ['K1','K2','K3','K4','K5','K6','K7','K8'];
+    var data = [["10","120","70","30","190","70","30","190"],["10","120","70","30","190","70","30","190"],["10","120","70","30","190","70","30","190"],["10","120","70","30","190","70","30","190"],["10","120","70","30","190","70","30","190"],["10","120","70","30","190","70","30","190"],["10","120","70","30","190","70","30","190"],["10","120","70","30","190","70","30","190"]];
     var datasets = [];
     var option;
 
     for(var j=0; j<name.length;j++){
       datasets.push({
         'label': name[j],
-        'backgroundColor': 'blue',
+        'backgroundColor': $scope.getcolor(1),
         'borderColor': 'grey',
         'borderWidth': 0.6,
         'pointBackgroundColor': 'red',
@@ -26,11 +37,15 @@ angular.module('starter.controllers', [])
         duration: 5000
       },
       barThickness: 10,
+      legend: {
+        position: 'bottom'
+
+      },
       scales: {
         xAxes: [{
-            stacked: true,
-            barPercentage: 0.6,
-            categoryPercentage: 0.2
+            stacked: false,
+            barPercentage: 0.2,
+            categoryPercentage: 0.3
         }],
         yAxes: [{
             stacked: false
